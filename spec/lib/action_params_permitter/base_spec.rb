4e6220c0f3af0f16c638480501127f388f17bc5d
Sergey Gernyak <sergeg1990@gmail.com>
2015-10-29 00:26:38 +0200

    Specing #permit

--- spec/lib/action_params_permitter/base_spec.rb
+++ spec/lib/action_params_permitter/base_spec.rb
@@ -10,4 +10,15 @@ describe ActionParamsPermitter::Base do
 
   it { is_expected.to respond_to :hash_for_permitting }
   it { is_expected.to respond_to :permit }
+
+  describe '#permit' do
+    it 'calls permit processor correctly' do
+      expect(ActionParamsPermitter::PermitProcessor).to(
+        receive(:new)
+        .with(:income_params, instance_of(ActionParamsPermitter::Builders::Main))
+        .and_return(double(:permit_processor, call: nil))
+      )
+      permitter.permit(:income_params)
+    end
+  end
 end
